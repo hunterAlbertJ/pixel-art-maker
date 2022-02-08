8c82fb7bc36a100ef2b0310a62fd04fe9fe52a91
hunterAlbertJ <albert.hunter13@gmail.com>
2022-02-08 16:56:58 -0600

    finished

--- script.js
+++ script.js
@@ -1,255 +1,248 @@
 // document.body.style.width = "100%";
 // document.body.style.height = "100%";
 
-var colorArray = ['black','silver','gray','white','maroon','red','purple','fuchsia','green','olive','yellow','navy','blue', 'teal','aqua'];
-var mouseColor = '';
-var parentDiv = document.createElement('ul');
-parentDiv.setAttribute('id', 'parentDiv');
+var colorArray = [
+  "black",
+  "silver",
+  "gray",
+  "white",
+  "maroon",
+  "red",
+  "purple",
+  "fuchsia",
+  "green",
+  "olive",
+  "yellow",
+  "navy",
+  "blue",
+  "teal",
+  "aqua",
+];
+var mouseColor = "";
+var parentDiv = document.createElement("ul");
+parentDiv.setAttribute("id", "parentDiv");
 
 document.body.appendChild(parentDiv);
 var mouseIsPressed = false;
-var dimension = prompt('What size board do you want?')
+var dimension = prompt("What size board do you want?");
 var scaleFactor = 100 / dimension;
 
-console.log('Board size: ' + dimension, 'Scale factor ' + scaleFactor);
+console.log("Board size: " + dimension, "Scale factor " + scaleFactor);
 
+function mouseEnter() {
+  if (mouseIsPressed) {
+    console.log("Pressed and entering");
+    var test = window.event.target;
 
-
-function mouseEnter(){
-    if (mouseIsPressed) {
-        console.log("Pressed and entering")
-        var test = window.event.target
-    
-    if(test.style.backgroundColor === "" || test.style.backgroundColor !== mouseColor) {
-        test.style.backgroundColor = mouseColor
+    if (
+      test.style.backgroundColor === "" ||
+      test.style.backgroundColor !== mouseColor
+    ) {
+      test.style.backgroundColor = mouseColor;
     }
+  } 
+}
 
-    } else {
-        // console.log('entering');
+function mouseDown() {
+  mouseIsPressed = true;
+  console.log("pressed");
+}
+
+function mouseUp() {
+  mouseIsPressed = false;
+}
+function alertTest() {
+  var currentPixel = window.event.target;
+  console.log(currentPixel.id);
+
+  if (
+    currentPixel.style.backgroundColor === "" ||
+    currentPixel.style.backgroundColor !== mouseColor
+  ) {
+    currentPixel.style.backgroundColor = mouseColor;
+  }
+}
+function colorSelect() {
+  console.log(window.event.target.style.backgroundColor);
+  mouseColor = window.event.target.style.backgroundColor;
+  currentColorText.innerHTML = mouseColor;
+  currentColorBox.style.backgroundColor = mouseColor;
+}
+function specificColor() {
+  console.log(document.getElementById("colorChoice").value);
+  var specifcSelect = document.getElementById("colorChoice");
+  specifcSelect.style.backgroundColor = specifcSelect.value;
+  mouseColor = specifcSelect.style.backgroundColor;
+  currentColorText.innerHTML = mouseColor;
+  currentColorBox.style.backgroundColor = mouseColor;
+}
+
+function reset() {
+  document.getElementsByTagName("UL")[0].innerHTML = "";
+  for (var i = 0; i < dimension; i++) {
+    for (var j = 0; j < dimension; j++) {
+      var divName = [];
+      divName[0] = i;
+      divName[1] = j;
+
+      var checker = document.createElement("div");
+      checker.setAttribute("id", divName);
+      checker.style.height = scaleFactor + "%";
+      checker.style.width = scaleFactor + "%";
+      checker.style.float = "left";
+      checker.style.paddingBottom = scaleFactor + "%";
+      checker.style.border = "inset black";
+      checker.style.borderWidth = "1px 1px";
+
+      checker.addEventListener("click", alertTest);
+      checker.addEventListener("mouseenter", mouseEnter);
+      checker.addEventListener("mousedown", mouseDown);
+      checker.addEventListener("mouseup", mouseUp);
+      document.getElementById("parentDiv").appendChild(checker);
     }
+  }
+}
+function save() {
+  var saveArray = [];
+  var tempArr = [];
+  saveArray = document.getElementsByTagName("DIV");
+  var arr = [].slice.call(saveArray);
+  for (var e = 0; e < arr.length; e++) {
+    tempArr.push(arr[e].style.backgroundColor);
+  }
+  console.log(tempArr);
+  localStorage.setItem("savedPainting", JSON.stringify(tempArr));
+}
+function load() {
+  var savedPaining = JSON.parse(localStorage.savedPainting);
+  for (var e = 0; e < savedPaining.length; e++) {
+    document.getElementsByTagName("DIV")[e].style.backgroundColor =
+      savedPaining[e];
+  }
 
-}
-
-function mouseDown(){
-    mouseIsPressed = true;
-    console.log('pressed');
-}
-
-function mouseUp(){
-    mouseIsPressed = false;
-}
-function alertTest(){
-    var currentPixel = window.event.target
-    console.log(currentPixel.id);
-    
-    if(currentPixel.style.backgroundColor === "" || currentPixel.style.backgroundColor !== mouseColor) {
-        currentPixel.style.backgroundColor = mouseColor
-    } 
-}
-function colorSelect(){
-    console.log(window.event.target.style.backgroundColor);
-    mouseColor = window.event.target.style.backgroundColor;
-    currentColorText.innerHTML = mouseColor;
-    currentColorBox.style.backgroundColor = mouseColor;
-    
-}
-function specificColor(){
-    console.log(document.getElementById("colorChoice").value);
-    var specifcSelect = document.getElementById('colorChoice')
-    specifcSelect.style.backgroundColor = specifcSelect.value;
-    mouseColor = specifcSelect.style.backgroundColor
-    
-
-    
-}
-
-function reset(){
-    document.getElementsByTagName("UL")[0].innerHTML = "";
-    for (var i = 0; i < dimension; i++) {
-    
-        for (var j = 0; j < dimension; j++) {
-          var divName = [];
-          divName[0] = i;
-          divName[1] = j;
-          
-          var checker = document.createElement("div");
-          checker.setAttribute('id', divName);
-          checker.style.height = scaleFactor + "%";
-          checker.style.width = scaleFactor + "%";
-          checker.style.float = "left";
-          checker.style.paddingBottom = scaleFactor + "%";
-          checker.style.border = "inset black";
-          checker.style.borderWidth="1px 1px"
-          
-      
-          checker.addEventListener("click", alertTest);
-          checker.addEventListener('mouseenter', mouseEnter);
-          checker.addEventListener('mousedown', mouseDown);
-          checker.addEventListener('mouseup', mouseUp);
-          document.getElementById('parentDiv').appendChild(checker);
-        }
-      }
-      
-}
-function save(){
-var saveArray = [];
-var tempArr = [];
-saveArray = document.getElementsByTagName("DIV")
-var arr = [].slice.call(saveArray);
-for(var e = 0; e < arr.length; e++){
-    
-    tempArr.push((arr[e].style.backgroundColor));
-};
-console.log(tempArr);
-
+  // (document.getElementsByTagName("DIV")[0].style.backgroundColor);
 }
 
 for (var i = 0; i < dimension; i++) {
-    
   for (var j = 0; j < dimension; j++) {
     var divName = [];
     divName[0] = i;
     divName[1] = j;
-    
+
     var checker = document.createElement("div");
-    checker.setAttribute('id', divName);
+    checker.setAttribute("id", divName);
     checker.style.height = scaleFactor + "%";
     checker.style.width = scaleFactor + "%";
     checker.style.float = "left";
     checker.style.paddingBottom = scaleFactor + "%";
     checker.style.border = "inset black";
-    checker.style.borderWidth="1px 1px"
-    
+    checker.style.borderWidth = "1px 1px";
 
     checker.addEventListener("click", alertTest);
-    checker.addEventListener('mouseenter', mouseEnter);
-    checker.addEventListener('mousedown', mouseDown);
-    checker.addEventListener('mouseup', mouseUp);
-    document.getElementById('parentDiv').appendChild(checker);
+    checker.addEventListener("mouseenter", mouseEnter);
+    checker.addEventListener("mousedown", mouseDown);
+    checker.addEventListener("mouseup", mouseUp);
+    document.getElementById("parentDiv").appendChild(checker);
   }
 }
 
-
 var colorTemplate = document.createElement("footer");
-colorTemplate.setAttribute('id', "foot")
+colorTemplate.setAttribute("id", "foot");
 document.body.appendChild(colorTemplate);
 console.log(colorTemplate);
 
-var colorinner = document.createElement('colorTemplate');
-colorinner.innerHTML = 'Color Template:' + '\n';
-colorinner.setAttribute('id', 'colortemplate');
+var colorinner = document.createElement("colorTemplate");
+colorinner.innerHTML = "Color Template:" + "\n";
+colorinner.setAttribute("id", "colortemplate");
 
-document.getElementById('foot').appendChild(colorinner);
+document.getElementById("foot").appendChild(colorinner);
 
+var currentColorText = document.createElement("colorText");
+currentColorText.innerHTML = "Current Color" + "\n";
+document.getElementById("colortemplate").appendChild(currentColorText);
 
-
-var currentColorText = document.createElement('colorText');
-currentColorText.innerHTML = 'Current Color' + '\n';
-document.getElementById('colortemplate').appendChild(currentColorText);
-
-var currentColorBox = document.createElement('CurrentColorBox');
-currentColorBox.setAttribute('id','CurrentColorBox');
+var currentColorBox = document.createElement("CurrentColorBox");
+currentColorBox.setAttribute("id", "CurrentColorBox");
 currentColorBox.style.height = "1.0%";
 currentColorBox.style.width = "1.0%";
 currentColorBox.style.float = "right";
 currentColorBox.style.paddingBottom = "1.0%";
 currentColorBox.style.border = "inset black";
-currentColorBox.style.borderWidth="1px 1px"
-currentColorBox.style.backgroundColor = ''
-currentColorBox.style.borderRadius = "50%"
-document.getElementById('colortemplate').append(currentColorBox);
+currentColorBox.style.borderWidth = "1px 1px";
+currentColorBox.style.backgroundColor = "";
+currentColorBox.style.borderRadius = "50%";
+document.getElementById("colortemplate").append(currentColorBox);
 
-
-
-
-
-
-
-for (var h = 0; h < colorArray.length; h++){
-    var colorTempName = [];
-    colorTempName[0] = h;
-    var colorSwatch = document.createElement('colorSwatch');
-    colorSwatch.setAttribute('id', colorTempName);
-    colorSwatch.style.height = "2.5%";
-    colorSwatch.style.width = "2.5%";
-    colorSwatch.style.float = "left";
-    colorSwatch.style.paddingBottom = "2.5%";
-    colorSwatch.style.border = "inset black";
-    colorSwatch.style.borderWidth="3px 3px"
-    colorSwatch.style.borderRadius = "50%"
-    colorSwatch.style.backgroundColor = colorArray[h];
-    colorSwatch.addEventListener("click", colorSelect);
-    document.getElementById('foot').appendChild(colorSwatch);
+for (var h = 0; h < colorArray.length; h++) {
+  var colorTempName = [];
+  colorTempName[0] = h;
+  var colorSwatch = document.createElement("colorSwatch");
+  colorSwatch.setAttribute("id", colorTempName);
+  colorSwatch.style.height = "2.5%";
+  colorSwatch.style.width = "2.5%";
+  colorSwatch.style.float = "left";
+  colorSwatch.style.paddingBottom = "2.5%";
+  colorSwatch.style.border = "inset black";
+  colorSwatch.style.borderWidth = "3px 3px";
+  colorSwatch.style.borderRadius = "50%";
+  colorSwatch.style.backgroundColor = colorArray[h];
+  colorSwatch.addEventListener("click", colorSelect);
+  document.getElementById("foot").appendChild(colorSwatch);
 }
 
-var userColor = document.createElement('input');
-userColor.setAttribute('id', 'colorChoice');
+var userColor = document.createElement("input");
+userColor.setAttribute("id", "colorChoice");
 userColor.style.height = "5.0%";
 userColor.style.width = "5.0%";
 userColor.style.float = "left";
 userColor.style.paddingBottom = "5.0%";
 userColor.style.border = "inset black";
-userColor.style.borderWidth="3px 3px"
-userColor.style.borderRadius = "50%"
-userColor.setAttribute('type', 'color');
+userColor.style.borderWidth = "3px 3px";
+userColor.style.borderRadius = "50%";
+userColor.setAttribute("type", "color");
 userColor.addEventListener("change", specificColor);
-document.getElementById('foot').appendChild(userColor);
+document.getElementById("foot").appendChild(userColor);
 
+var resolution = 20 * dimension + dimension * 2;
+document.getElementById("parentDiv").style.maxWidth = resolution + "px";
+document.getElementById("parentDiv").style.maxHeight = resolution + "px";
+console.log(document.getElementById("parentDiv").style.maxHeight);
 
-var resolution = (20 * dimension) + (dimension * 2)
-document.getElementById('parentDiv').style.maxWidth = resolution + 'px'
-document.getElementById('parentDiv').style.maxHeight = resolution + 'px'
-console.log(document.getElementById('parentDiv').style.maxHeight);
-
-var resetButton = document.createElement('button');
-resetButton.setAttribute('id', 'reset');
+var resetButton = document.createElement("button");
+resetButton.setAttribute("id", "reset");
 resetButton.style.height = "2.0%";
 resetButton.style.width = "5.0%";
 resetButton.style.float = "left";
 resetButton.style.paddingBottom = "5.0%";
 resetButton.style.border = "inset black";
-resetButton.style.borderWidth="3px 3px"
+resetButton.style.borderWidth = "3px 3px";
 
-resetButton.innerHTML = 'RESET'
+resetButton.innerHTML = "RESET";
 resetButton.addEventListener("click", reset);
-document.getElementById('foot').appendChild(resetButton);
+document.getElementById("foot").appendChild(resetButton);
 
-
-
-
-
-var resetButton = document.createElement('button');
-resetButton.setAttribute('id', 'save');
+var resetButton = document.createElement("button");
+resetButton.setAttribute("id", "save");
 resetButton.style.height = "2.0%";
 resetButton.style.width = "5.0%";
 resetButton.style.float = "left";
 resetButton.style.paddingBottom = "5.0%";
 resetButton.style.border = "inset black";
-resetButton.style.borderWidth="3px 3px"
+resetButton.style.borderWidth = "3px 3px";
 
-resetButton.innerHTML = 'SAVE'
+resetButton.innerHTML = "SAVE";
 resetButton.addEventListener("click", save);
-document.getElementById('foot').appendChild(resetButton);
+document.getElementById("foot").appendChild(resetButton);
 
+var resetButton = document.createElement("button");
+resetButton.setAttribute("id", "load");
+resetButton.style.height = "2.0%";
+resetButton.style.width = "5.0%";
+resetButton.style.float = "left";
+resetButton.style.paddingBottom = "5.0%";
+resetButton.style.border = "inset black";
+resetButton.style.borderWidth = "3px 3px";
 
-
-// var saveButtton = document.createElement('buttons');
-// saveButton.setAttribute('id', 'save');
-// saveButton.style.height = "2.0%";
-// saveButton.style.width = "5.0%";
-// saveButton.style.float = "left";
-// saveButton.style.paddingBottom = "5.0%";
-// saveButton.style.border = "inset black";
-// saveButton.style.borderWidth="3px 3px"
-// saveButton.innerHTML = 'SAVE'
-// saveButton.addEventListener("click", save);
-// document.getElementById('foot').appendChild(saveButton);
-
-
-// var saveArray = [];
-// saveArray = document.getElementsByTagName("DIV")
-// var arr = [].slice.call(saveArray);
-// for(var e = 0; e < arr.length; e++){
-//     console.log(arr[e].style.backgroundColor);
-// };
-
+resetButton.innerHTML = "LOAD";
+resetButton.addEventListener("click", load);
+document.getElementById("foot").appendChild(resetButton);
